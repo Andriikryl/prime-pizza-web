373c65b3252193355ccff6e927e2520c1ba2e714
Andriikryl <andreyk557@gmail.com>
2023-08-29 17:16:02 +0300

    add hero section component

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Container } from "@/components/container/Container";
+import style from "./style.module.css";
+import Button from "@/components/button/Button";
 
 export default function Hero() {
-  return <div>рукщ</div>;
+  return (
+    <section className={style.hero}>
+      <Container className={style.hero__container}>
+        <div className={style.hero__box}>
+          <h1 className={style.hero__title}>
+            Сумніваєшся, яку піцу вибрати? Тицяй на піцу, та крути колесо!
+          </h1>
+          <p className={style.hero__description}>
+            Lorem Ipsum в основному частиною латинського тексту за класичною
+            автор і філософа Цицерона. Це слова і букви були змінені додаванням
+            або видаленням, так навмисно роблять.
+          </p>
+          <Button>Дивитись меню</Button>
+        </div>
+      </Container>
+    </section>
+  );
 }
